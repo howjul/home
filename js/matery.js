bb65aa2be5cbdb3424b7aebe8c919a09b495ff3a
Haozhe Zhu <zjuzhz@outlook.com>
2023-04-19 23:49:27 +0800

    Site updated: 2023-04-19 23:49:27

--- js/matery.js
+++ js/matery.js
@@ -106,6 +106,11 @@ $(function () {
             subHtmlSelectorRelative: true
         });
 
+        //自己加的，图片懒加载
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
+    
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
